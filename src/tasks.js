bda78cab59d83c5b275fa2790a17178b31222780
Alvaro Castillo <alvarolucascc96@gmail.com>
2023-01-22 00:04:25 -0300

    feat(Tasks): Basic component for Tasks List

---
+++ src/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Example Task 1",
+    description: "Description Task 1",
+  },
+  {
+    id: 1,
+    title: "Example Task 2",
+    description: "Description Task 2",
+  },
+  {
+    id: 2,
+    title: "Example Task 3",
+    description: "Description Task 3",
+  },
+];
