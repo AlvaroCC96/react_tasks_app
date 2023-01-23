dce2337743e0877b8b01fe7068d44d29b54b9fa7
Alvaro Castillo <alvarolucascc96@gmail.com>
2023-01-23 02:52:55 -0300

    style(TaskList): change style for mobile view

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -9,7 +9,7 @@ function TaskList() {
   }
 
   return (
-    <div className="grid grid-cols-4 gap-2">
+    <div className="grid grid-cols-1 gap-2 text-center md:grid-cols-4">
       {tasks.map((task) => (
         <TaskCard task={task} key={task.id}/>
       ))}
