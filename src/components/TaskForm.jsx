765476a3806afe22ce57e97bfa4f90b3807db5b7
Alvaro Castillo <alvarolucascc96@gmail.com>
2023-01-23 01:03:31 -0300

    feat(Task): create new Task

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function TaskForm({ createTask }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createTask({
+      title,
+      description,
+    });
+    setTitle("");
+    setDescription("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Escribe tu tarea"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        autoFocus
+      />
+      <textarea
+        placeholder="Escribe tu descripción"
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+      ></textarea>
+      <button>Guardar</button>
+    </form>
+  );
+}
