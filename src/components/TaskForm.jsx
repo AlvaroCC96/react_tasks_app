fe70497b41ee5737a2729f3cb296729f5f13693f
Alvaro Castillo <alvarolucascc96@gmail.com>
2023-01-23 02:23:02 -0300

    feat(Tailwind): style implemented

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -18,19 +18,24 @@ export default function TaskForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <div className="max-w-md mx-auto">
+      <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4">
+        <h1 className="text-2xl font-bold text-white mb-3">Crea tu tarea</h1>
       <input
         placeholder="Escribe tu tarea"
         onChange={(e) => setTitle(e.target.value)}
         value={title}
         autoFocus
+        className="bg-slate-300 p-3 w-full mb-2"
       />
       <textarea
         placeholder="Escribe tu descripción"
         onChange={(e) => setDescription(e.target.value)}
         value={description}
+        className="bg-slate-300 p-3 w-full mb-2"
       ></textarea>
-      <button>Guardar</button>
+      <button className="bg-indigo-500 px-3 py-1 text-white hover:bg-indigo-700">Guardar</button>
     </form>
+    </div>
   );
 }
